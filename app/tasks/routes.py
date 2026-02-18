269ff4d1dcc3bbde517399986c4d479965ec799b
Saman Hovar <samanghasemidesk@gmail.com>
2026-02-18 04:44:21 +0330

    tasks: routes & schemas created but not tested

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -1,29 +1,101 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query, Depends, HTTPException, status
 from typing import Annotated
+from tasks.schemas import TaskCreateSchema, TaskResponseSchema, TaskUpdateSchema
+from tasks.models import TaskModel
+
+# sqlalchemy imports
+from sqlalchemy.orm import Session
+from core.database import get_db
+
 
 router = APIRouter(tags=["tasks"])
 
 
-@router.get("/tasks")
-async def retrevie_task_list():
-    return []
+@router.get(
+    "/tasks", response_model=list[TaskResponseSchema], status_code=status.HTTP_200_OK
+)
+async def retrevie_task_list(
+    completed: Annotated[
+        bool, Query(None, description="filter tasks based on being completed or not")
+    ],
+    limit: Annotated[
+        int,
+        Query(10, gt=0, le=50, description="limiting the number of items to retrieve"),
+    ],
+    offset: Annotated[
+        int, Query(0, ge=0, description="use for paginating based on passed items")
+    ],
+    db: Annotated[Session, Depends(get_db)],
+):
+    query = db.query(TaskModel)
+    if completed is not None:
+        query = query.filter_by(is_completed=completed)
+
+    return query.limit(limit).offset(offset).all()
 
 
-@router.get("/tasks/{task_id}")
-async def retrevie_single_task_detail(task_id: Annotated[int, Path(..., gt=0)]):
-    return {}
+@router.get(
+    "/tasks/{task_id}",
+    response_model=TaskResponseSchema,
+    status_code=status.HTTP_200_OK,
+)
+async def retrevie_single_task_detail(
+    task_id: Annotated[int, Path(..., gt=0)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    task_obj = db.query(TaskModel).filter_by(id=task_id).first()
+    if not task_id:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task_obj
 
 
-@router.post("/tasks")
-async def create_task():
-    return {}
+@router.post(
+    "/tasks", response_model=TaskResponseSchema, status_code=status.HTTP_201_CREATED
+)
+async def create_task(
+    request: TaskCreateSchema,
+    db: Annotated[Session, Depends(get_db)],
+):
+    task_obj = TaskModel(**request.model_dump())
+    db.add(task_obj)
+    db.commit()
+    db.refresh(task_obj)
+
+    return task_obj
 
 
-@router.put("/tasks/{task_id}")
-async def update_task(task_id: Annotated[int, Path(..., gt=0)]):
-    return {}
+@router.put(
+    "/tasks/{task_id}",
+    response_model=TaskResponseSchema,
+    status_code=status.HTTP_200_OK,
+)
+async def update_task(
+    request: TaskUpdateSchema,
+    task_id: Annotated[int, Path(..., gt=0)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    task_obj = db.query(TaskModel).filter_by(id=task_id).first()
+    if not task_id:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    # Update fields using setattr
+    for field, value in request.model_dump(exclude_unset=True).items():
+        setattr(task_obj, field, value)
+
+    db.commit()
+    db.refresh(task_obj)
+
+    return task_obj
 
 
-@router.delete("/tasks/{task_id}")
-async def delete_task(task_id: Annotated[int, Path(..., gt=0)]):
-    return {}
+@router.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(
+    task_id: Annotated[int, Path(..., gt=0)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    task_obj = db.query(TaskModel).filter_by(id=task_id).first()
+    if not task_obj:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task_obj)
+    db.commit()
---
+++ app/tasks/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import Annotated
+from datetime import datetime
+
+
+class TaskBaseSchema(BaseModel):
+    title: Annotated[
+        str, Field(..., max_length=150, min_length=3, description="Title of the task")
+    ]
+    description: Annotated[
+        str | None, Field(max_length=500, description="Description of the task")
+    ]
+    is_completed: Annotated[bool, Field(..., description="State of the task")]
+
+
+class TaskCreateSchema(TaskBaseSchema):
+    pass
+
+
+class TaskUpdateSchema(TaskBaseSchema):
+    pass
+
+
+class TaskResponseSchema(TaskBaseSchema):
+    id: Annotated[int, Field(..., description="Unique identifier of the object")]
+
+    created_date: Annotated[
+        datetime, Field(..., description="Creation date and time of the object")
+    ]
+    updated_date: Annotated[
+        datetime, Field(..., description="Updating date and time of the object")
+    ]
