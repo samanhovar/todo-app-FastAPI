c5c1a47f40484ec805df8ef8b2a5b4733b63d0ea
Saman Hovar <samanghasemidesk@gmail.com>
2026-02-19 14:15:28 +0330

    test api key query authentication & api key header authentication

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi.dependencies.models import Dependant
 from typing import Annotated
 from fastapi import FastAPI, Depends
+from fastapi.security import APIKeyHeader, APIKeyQuery
 from contextlib import asynccontextmanager
 from tasks.routes import router as task_router
 from users.routes import router as user_router
@@ -60,3 +60,17 @@ def public_route():
 @app.get("/private")
 def private_route(user: Annotated[UserModel, Depends(get_authenticated_user)]):
     return {"message": "This is a private route."}
+
+
+header_schema = APIKeyHeader(name="x-key")
+
+@app.get("/apikeyheader")
+def api_key_header(api_key = Depends(header_schema)):
+    return {"message": "This is a test for APIKeyHeader"}
+
+
+query_schema = APIKeyQuery(name="api-key")
+
+@app.get("/apikeyquery")
+def api_key_query(api_key = Depends(query_schema)):
+    return {"message": "This is a test for APIKeyQuery"}
